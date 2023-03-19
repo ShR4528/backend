c11dac4e97a27633186b0e2a5dd2a48a493f1fd0
Shr4528 <Shr4528@gmail.com>
2023-03-19 12:45:15 -0500

    db

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -18,3 +19,14 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log('Server running on http://localhost:8080/');
+});
+
+const MONGO_URL =
+  'mongodb+srv://shamil17:shamil17@cluster0.lr3qlvu.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
