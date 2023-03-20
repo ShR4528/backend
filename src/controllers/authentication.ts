641a9e25400ad83a39d6231d213bd26073128beb
Shr4528 <Shr4528@gmail.com>
2023-03-20 09:48:42 -0500

    login

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,6 +1,39 @@
 import express from 'express';
+
 import { getUserByEmail, createUser } from '../db/users';
 import { random, authentication } from '../helpers';
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      '+authentication.salt + authenticatio.password'
+    );
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password != expectedHash) {
+      return res.sendStatus(403);
+    }
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
 
 export const register = async (req: express.Request, res: express.Response) => {
   try {
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,19 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
-import mongoose from 'mongoose';
+//import mongoose from 'mongoose';
 import router from './router';
+
+const mongoose = require('mongoose');
+mongoose.connect(
+  'mongodb+srv://shamil17:shamil17@cluster0.lr3qlvu.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true }
+);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected to MongoDB!');
+});
 
 const app = express();
 
